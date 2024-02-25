fae2149f0a25dbf204406b5cdc6ea3ed7954e1d8
Jorge Medina <medidrones@gmail.com>
2024-02-25 17:20:05 -0300

    Actualizar password por token

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,6 +5,7 @@ import { productPaginationReducer } from "./slices/productPaginationSlice";
 import { categoryReducer } from "./slices/categorySlice";
 import { securityReducer } from "./slices/securitySlice";
 import { forgotPasswordReducer } from "./slices/forgotPasswordSlice";
+import { resetPasswordReducer } from "./slices/resetPasswordSlice";
 
 export default configureStore({
   reducer: {
@@ -14,6 +15,7 @@ export default configureStore({
     category: categoryReducer,
     security: securityReducer,
     forgotPassword: forgotPasswordReducer,
+    resetPassword: resetPasswordReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
